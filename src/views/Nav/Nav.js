98b4e4a3b8b7e26d49ccc06924f57ef5b8ee4e0f
Phung Duc Hung <phungduchung9808@gmail.com>
2024-05-29 16:50:16 +0700

    Link & NavLink ( do  not use  tag a )

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,14 +1,31 @@
 import React from "react";
 import './Nav.scss';
+import {
+    Link , NavLink
+  } from "react-router-dom";
+
 class Nav extends React.Component {
     render() {
         return (
             <div className="topnav">
-            <a className="active" href="/">Home</a>
-            <a href="/todo">Todos</a>
-            <a href="/about">About</a>
+                <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
+                <NavLink to="/todo" activeClassName="active" exact={true}>Todos</NavLink>
+                <NavLink to="/about" activeClassName="active" exact={true}>About</NavLink>
+
+                {/* <Link to="/">Home</Link>
+                <Link to="/todo">Todos</Link>
+                <Link to="/about">About</Link> */}
             </div>
         )
     }
 }
 export default Nav;
+
+
+
+
+
+
+
+
+
